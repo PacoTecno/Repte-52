deee2282a5c89d56166b89789e667a4e3fb96938
PacoTecno <99887378+PacoTecno@users.noreply.github.com>
2022-03-12 13:41:50 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,23 @@
+input.onButtonPressed(Button.A, function () {
+    Personas = Personas + 1
+    basic.showNumber(Personas)
+})
+input.onGesture(Gesture.Shake, function () {
+    Personas = 0
+    basic.showNumber(Personas)
+})
+input.onButtonPressed(Button.B, function () {
+    if (Personas > 0) {
+        Personas += -1
+        basic.showNumber(Personas)
+    }
+})
+let Personas = 0
+Personas = 0
+basic.forever(function () {
+    if (Personas > 5) {
+        basic.showString("AF")
+    } else {
+        basic.showNumber(Personas)
+    }
+})
